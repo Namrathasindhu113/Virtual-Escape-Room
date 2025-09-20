d4f7b427b7823ab7ada1acaa18f2cf29ef5e2360
Namratha Sindhu <namrathasindhu113@gmail.com>
2025-09-20 12:01:03 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,9 +1,10 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
-import { getFirestore, Firestore } from "firebase/firestore";
-import { getAuth, Auth } from "firebase/auth";
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
+// This is a public configuration and is safe to be exposed.
 const firebaseConfig = {
   apiKey: "AIzaSyA1upQpYN0dRtoterK2b60PRZu00idto7w",
   authDomain: "studio-7794488728-5e169.firebaseapp.com",
@@ -11,13 +12,12 @@ const firebaseConfig = {
   storageBucket: "studio-7794488728-5e169.appspot.com",
   messagingSenderId: "527192490995",
   appId: "1:527192490995:web:1876f1f0316c764ad04886",
-  measurementId: ""
 };
 
 // Initialize Firebase for Singleton Pattern
-const app: FirebaseApp = getApps().length ? getApp() : initializeApp(firebaseConfig);
-const db: Firestore = getFirestore(app);
-const auth: Auth = getAuth(app);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
 
 
 export { app, db, auth };
